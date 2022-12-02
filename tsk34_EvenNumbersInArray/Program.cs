21b426d96cf1f6bc504f589df8c1392212edd468
Shoshela <goyati@mail.ru>
2022-12-02 14:04:16 +0300

    Modify tsk34_EvenNumbersInArray

--- tsk34_EvenNumbersInArray/Program.cs
+++ tsk34_EvenNumbersInArray/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int arSize = ReadArSize("Enter the dimension of the array ... ");
+int [] numbers = new int[arSize];
+
+ArrayFillingRandomNumbers(numbers);
+PrintArray(numbers);
+int evenElements = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        evenElements++;
+    }
+}
+if (evenElements == 1)
+{
+    Console.WriteLine($"There is {evenElements} even number in the array");
+}
+else
+    Console.WriteLine($"There are {evenElements} even numbers in the array");
+
+// Методы
+
+int ReadArSize(string message)  // Функция ввода размерности массива
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ArrayFillingRandomNumbers(int [] array) // Функция заполнения массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array) // Функция вывода массива на экран
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine("]");
+}
